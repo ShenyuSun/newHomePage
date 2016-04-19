61ae664e7f07793eb705a45e2e5b6c0d1c687a26
ShenyuSun <shenyu0930@gmail.com>
2016-04-19 15:05:51 -0400

    Latest version
    Adding picture and animation to the new home page.

---
+++ app.js
@@ -0,0 +1,42 @@
+//-------------------------------------------------
+// DOCUMENT READY
+//-------------------------------------------------
+
+$(document).ready(function() {
+
+    //---------------------------------------------
+    //      Like
+    //---------------------------------------------
+    $(".js-like").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).text("Liked")
+            .closest(".newsite-item")
+            .addClass("is-liked");
+    });
+
+    //---------------------------------------------
+    //      Link TextArea
+    //---------------------------------------------
+    $(".js-add-link").on("click", function(event) {
+        event.preventDefault();
+
+        $(".js-form").toggleClass("is-visible");
+    });
+
+    //-----------------------------------------------
+    //      Show up picture
+    //-----------------------------------------------
+
+    $('.js-show-modal').on('click', function(event) {
+        event.preventDefault();
+
+        $('.js-modal').addClass('is-visible');
+        $('.js-modal-overlay').addClass('is-visible');
+    });
+
+    $('.js-modal-overlay').on('click', function(event) {
+        $('.js-modal').removeClass('is-visible');
+        $('.js-modal-overlay').removeClass('is-visible');
+    });
+});
